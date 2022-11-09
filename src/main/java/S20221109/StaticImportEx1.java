6775b327eb39c6ba9434ef50fd00033ecb514b5c
kimSM94 <rnentkdals@gmail.com>
2022-11-09 23:21:58 +0900

    20221031 Java Study

---
+++ src/main/java/S20221109/StaticImportEx1.java
@@ -0,0 +1,12 @@
+package S20221109;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+class StaticImportEx1 {
+    public static void main(String[] args) {
+        out.println(random());
+        out.println("Math.PI : " +PI);
+    }
+
+}
